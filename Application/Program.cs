41d65fc9da6bacdb0da25f63dbb8ec9e577de612
Marco Kreeft <marco.kreeft@gmail.com>
2025-03-12 21:02:32 +0100

    Added docker support

--- Application/Program.cs
+++ Application/Program.cs
@@ -22,13 +22,8 @@ builder.Services.AddScoped<SpoolmanClient>();
 var app = builder.Build();
     
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
