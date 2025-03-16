7e2d83e88057e2c3f102e9de2ae89e2358239a1c
Marco Kreeft <marco.kreeft@gmail.com>
2025-03-16 19:11:55 +0100

    Update filament color and adjust test queries
    Changed filament color from "FFFFFF" to "FFFF00" and updated the corresponding mock API query string. Removed unnecessary commented-out code related to a PUT request for spool usage.

--- Gateways.Tests/Spoolman/Endpoints/FilamentTests.cs
+++ Gateways.Tests/Spoolman/Endpoints/FilamentTests.cs
@@ -7,7 +7,7 @@ namespace Gateways.Tests
     internal class FilamentTests : EndpointTest<FilamentSpoolManEndpoint>
     {
         private Vendor vendor = new Vendor() { Id = 1, Name = "Vendor1" };
-        private Filament filament = new Filament() { Id = 2, Name = "Vendor1", Material = "PLA", ColorHex = "FFFFFF" };
+        private Filament filament = new Filament() { Id = 2, Name = "Vendor1", Material = "PLA", ColorHex = "FFFF00" };
 
         [Test]
         public async Task GivenNonExistingFilament_WhenGetOrCreate_CreatedFilamentldBeReturned()
@@ -43,7 +43,6 @@ namespace Gateways.Tests
                .WithQueryString(FilamentQueryConstants.Material, "PLA")
                .Respond("application/json", "[{\"id\":2,\"registered\":\"2025-03-15T15:18:26Z\",\"name\":\"Gray\",\"vendor\":{\"id\":1,\"registered\":\"2025-03-10T20:38:35Z\",\"name\":\"Vendor1\",\"external_id\":\"Vendor1\",\"extra\":{}},\"material\":\"PLA\",\"price\":0.0,\"density\":1.24,\"diameter\":1.75,\"weight\":1000.0,\"spool_weight\":0.0,\"color_hex\":\"898989\",\"extra\":{}},{\"id\":3,\"registered\":\"2025-03-15T15:18:49Z\",\"name\":\"Peru\",\"vendor\":{\"id\":1,\"registered\":\"2025-03-10T20:38:35Z\",\"name\":\"Vendor1\",\"external_id\":\"Vendor1\",\"extra\":{}},\"material\":\"PLA\",\"price\":0.0,\"density\":1.24,\"diameter\":1.75,\"weight\":1000.0,\"spool_weight\":0.0,\"color_hex\":\"FFFFFF\",\"extra\":{}}]");
 
-
             mockHandler
                .When("/api/v1/filament")
                .WithQueryString(FilamentQueryConstants.VendorName, "Vendor1")
@@ -55,11 +54,6 @@ namespace Gateways.Tests
                .When("/api/v1/filament")
                .WithContent("{\"id\":0,\"name\":\"Yellow\",\"vendor_id\":1,\"material\":\"PLA\",\"price\":0,\"density\":1.24,\"diameter\":1.75,\"weight\":1000,\"spool_weight\":0,\"extruder_temp\":0,\"bed_temp\":0,\"color_hex\":\"FFFF00\"}")
                .Respond("application/json", JsonSerializer.Serialize(filament));
-
-            //mockHandler
-            //    .When(HttpMethod.Put, "/api/v1/spool/2/use")
-            //    .WithContent("{\"use_weight\":100}")
-            //    .Respond("application/json", "");
         }
     }
 }
